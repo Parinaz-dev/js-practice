e0087ff1c6c00aca8dda11eea0dbb3a1a664f696
parinaz <p.ebrahimi520@gmail.com>
2024-12-03 09:51:48 -0800

    update file index.html

---
+++ js/script.js
@@ -0,0 +1,24 @@
+for (var data = 1; data <= 10; data++) {
+    var b = data % 2;
+    if (b == 0) {
+        console.log(data);
+
+        console.log("این عدد زوج است");
+
+
+        //کنسول گرفتم از بی چیزی نشون نداد
+    }
+    /*     else (b == 1); {بی نیاز نبود
+    
+            console.log(data);
+            console.log("این عدد فرد است");
+        }*/
+
+}
+
+
+
+
+
+
+
